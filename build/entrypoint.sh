d87de0d67cdc5307fe4dfd9ad6a69dd9762eea64
Il`ya Semyonov <Ilya@marshal.by>
2021-02-18 23:00:58 +0100

    auditwheel + several python versions

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
-set -e -x
+set -e -u -x
 
 PYTHON_VERSIONS=$1
 MANYLINUX_PLATFORM=$2
 
 cp -R /github/workspace /usr/src/tgcalls
-
-cd /usr/src
-echo $(ls)
-
 cd /usr/src/tgcalls
 
-#python3 setup.py build --debug
-/opt/python/cp37-cp37m/bin/python setup.py build --debug
-# TODO path from manylinux. Matrix with python versions
+function repair_wheel {
+    wheel="$1"
+    if ! auditwheel show "$wheel"; then
+        echo "Skipping non-platform wheel $wheel"
+    else
+        auditwheel repair "$wheel" --plat "$MANYLINUX_PLATFORM" -w /io/wheelhouse/
+    fi
+}
 
-# for by python version from args
-# build bwheel
-# auditwheel
+arrPYTHON_VERSIONS=(${PYTHON_VERSIONS// / })
+for PYTHON_VER in "${arrPYTHON_VERSIONS[@]}"; do
+    /opt/python/"${PYTHON_VER}"/bin/pip install --upgrade --no-cache-dir pip
+
+    /opt/python/"${PY_VER}"/bin/pip wheel /io/ --no-deps -w wheelhouse/
+done
+
+for whl in wheelhouse/*.whl; do
+    repair_wheel "$whl"
+done
+
+#/opt/python/cp37-cp37m/bin/python setup.py build --debug
+
 # upload to pypi/github releases/etc
