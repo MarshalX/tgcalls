7c5d23a710df4e77212bb9e27542227422c80759
Il`ya Semyonov <Ilya@marshal.by>
2021-02-18 22:00:24 +0100

    try first lib build on manylinux

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -7,8 +7,8 @@ MANYLINUX_PLATFORM=$2
 cd /github/workspace
 
 
-# RUN python3 setup.py build --debug
-#RUN /opt/python/cp37-cp37m/bin/python setup.py build --debug
+#python3 setup.py build --debug
+/opt/python/cp37-cp37m/bin/python setup.py build --debug
 # TODO path from manylinux. Matrix with python versions
 
 # for by python version from args
