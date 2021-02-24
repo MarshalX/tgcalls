ddf19662e391996acd4c63411f7cb7a4c3afe5a0
Il`ya Semyonov <Ilya@marshal.by>
2021-02-24 22:37:11 +0100

    add entrypoint file

---
+++ build/manylinux/entrypoint/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e -u -x
+
+PYTHON_VERSIONS=$1
+MANYLINUX_PLATFORM=$2
+
+export TWINE_USERNAME=$3
+export TWINE_PASSWORD=$4
+
+cp -R /github/workspace /usr/src/tgcalls
+cp -R /usr/src/Libraries/ /tmp/Libraries
+cd /usr/src/tgcalls
+
+function repair_wheel {
+    wheel="$1"
+    if ! auditwheel show "$wheel"; then
+        echo "Skipping non-platform wheel $wheel"
+    else
+        auditwheel repair "$wheel" --plat "$MANYLINUX_PLATFORM" -w wheelhouse/
+    fi
+}
+
+arrPYTHON_VERSIONS=(${PYTHON_VERSIONS// / })
+for PYTHON_VER in "${arrPYTHON_VERSIONS[@]}"; do
+    /opt/python/"${PYTHON_VER}"/bin/pip wheel . --no-deps -w wheelhouse/
+done
+
+for whl in wheelhouse/*.whl; do
+    repair_wheel "$whl"
+done
+
+/opt/python/cp37-cp37m/bin/pip install twine
+/opt/python/cp37-cp37m/bin/python -m twine upload /usr/src/tgcalls/wheelhouse/*-manylinux*.whl
