472f3ac1c94418cce1d49ae67f36077f48a0b8e8
Il'ya (Marshal) <ilya@marshal.by>
2021-07-20 17:09:20 +0200

    update examples for Telethon

--- examples/device_playout.py
+++ examples/device_playout.py
@@ -2,6 +2,9 @@ import os
 import asyncio
 
 import pytgcalls
+
+# choose one of this
+import telethon
 import pyrogram
 
 # EDIT VALUES!
@@ -10,14 +13,21 @@ API_ID = None
 CHAT_ID = '@tgcallschat'
 INPUT_DEVICE_NAME = 'MacBook Air Microphone'
 OUTPUT_DEVICE_NAME = 'MacBook Air Speakers'
+CLIENT_TYPE = pytgcalls.GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM
+# for Telethon uncomment line below
+# CLIENT_TYPE = pytgcalls.GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON
 
 
 async def main(client):
+    # its for Pyrogram
     await client.start()
     while not client.is_connected:
         await asyncio.sleep(1)
+    # for Telethon you can use this one:
+    # client.start()
 
-    group_call = pytgcalls.GroupCallFactory(client).get_device_group_call(INPUT_DEVICE_NAME, OUTPUT_DEVICE_NAME)
+    group_call = pytgcalls.GroupCallFactory(client, CLIENT_TYPE)\
+        .get_device_group_call(INPUT_DEVICE_NAME, OUTPUT_DEVICE_NAME)
     await group_call.start(CHAT_ID)
 
     # to get available device names
@@ -28,11 +38,19 @@ async def main(client):
 
 
 if __name__ == '__main__':
+    tele_client = telethon.TelegramClient(
+        os.environ.get('SESSION_NAME', 'pytgcalls'),
+        int(os.environ['API_ID']),
+        os.environ['API_HASH']
+    )
     pyro_client = pyrogram.Client(
         os.environ.get('SESSION_NAME', 'pytgcalls'),
         api_hash=os.environ.get('API_HASH', API_HASH),
         api_id=os.environ.get('API_ID', API_ID),
     )
 
+    # set your client (Pyrogram or Telethon)
+    main_client = pyro_client
+
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(pyro_client))
+    loop.run_until_complete(main(main_client))
--- examples/restream_using_raw_data.py
+++ examples/restream_using_raw_data.py
@@ -3,6 +3,9 @@ import os
 from typing import Optional
 
 from pytgcalls import GroupCallFactory
+
+# choose one of this
+import telethon
 import pyrogram
 
 # EDIT VALUES!
@@ -10,6 +13,9 @@ API_HASH = None
 API_ID = None
 PEER_ID = '@tgcallschat'  # chat or channel where you want to get audio
 PEER_ID2 = '@tgcallslib'  # chat or channel where you want to play audio
+CLIENT_TYPE = GroupCallFactory.MTPROTO_CLIENT_TYPE.PYROGRAM
+# for Telethon uncomment line below
+# CLIENT_TYPE = pytgcalls.GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON
 
 # global variable to transfer audio data between voice chats
 # it's not a good implementation, but its works and so easy to understand
@@ -26,11 +32,14 @@ def on_recorded_data(_, data: bytes, length: int) -> None:
 
 
 async def main(client):
+    # its for Pyrogram
     await client.start()
     while not client.is_connected:
         await asyncio.sleep(1)
+    # for Telethon you can use this one:
+    # client.start()
 
-    group_call_factory = GroupCallFactory(client)
+    group_call_factory = GroupCallFactory(client, CLIENT_TYPE)
 
     # handle input audio data from the first peer
     group_call_from = group_call_factory.get_raw_group_call(on_recorded_data=on_recorded_data)
@@ -43,11 +52,19 @@ async def main(client):
 
 
 if __name__ == '__main__':
+    tele_client = telethon.TelegramClient(
+        os.environ.get('SESSION_NAME', 'pytgcalls'),
+        int(os.environ['API_ID']),
+        os.environ['API_HASH']
+    )
     pyro_client = pyrogram.Client(
         os.environ.get('SESSION_NAME', 'pytgcalls'),
         api_hash=os.environ.get('API_HASH', API_HASH),
         api_id=os.environ.get('API_ID', API_ID),
     )
 
+    # set your client (Pyrogram or Telethon)
+    main_client = pyro_client
+
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(pyro_client))
+    loop.run_until_complete(main(main_client))
